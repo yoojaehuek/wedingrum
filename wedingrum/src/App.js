6f8cf3dc6d22bc20e0184c4334a262dca70a161f
김지환 <k1004902@gmail.com>
2023-11-09 22:38:01 +0900

    master3

--- wedingrum/src/App.js
+++ wedingrum/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
+import Header from './components/Header/Header';
+import Main from './pages/Main/Main';
+import Footer from './components/Footer/Footer';
 import './App.css';
 
 function App() {
---
+++ wedingrum/src/hooks/useAsync.js
@@ -0,0 +1,66 @@
+import { useReducer, useEffect } from "react"
+const initialState = {
+    loading: false,
+    data: null,
+    error: null
+}
+// 로딩중? 데이터 받기 성공? 데이터 받기 실패
+// LOADING , SUCCESS, ERROR
+function reducer(state, action){
+    switch(action.type) {
+        case "LOADING":
+            return {
+                loading: true,
+                data: null,
+                error: null
+            };
+        case "SUCCESS":
+            return {
+                loading: false,
+                data: action.data,
+                error: null
+            }
+        case "ERROR":
+            return {
+                loading: false,
+                data: null,
+                error: action.error
+            }
+        default:
+            return state;
+    }
+}
+
+function useAsync(callback, deps = []) {
+    // console.log("useAsync진입: ", deps);
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const fetchDate = async () => {
+        dispatch({type: "LOADING"});
+        try {
+            const data = await callback(); //callback은 getProduct()
+            console.log("어싱크data: ", data);
+            dispatch({
+                type: "SUCCESS",
+                data: data
+            })
+            if(data==false){
+                dispatch({
+                    type: "ERROR",
+                    error: "강제에러"
+                })  
+            }
+        }
+        catch(e){
+            dispatch({
+                type: "ERROR",
+                error: e
+            })
+        }
+    }
+    useEffect(()=>{ // useEffect(()=>{}, [])는 처음 1회만 실행 
+        fetchDate();
+    // eslint-disable-next-line
+    }, deps); //deps는 빈배열([]) 34라인
+    return [state, fetchDate];
+}
+export default useAsync
