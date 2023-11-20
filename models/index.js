1f6f32a91b5f81a72fda1b8958ac20b55a701b4d
yoojh <jaehek2629@naver.com>
2023-11-20 20:50:21 +0900

    플레너모델

--- models/index.js
+++ models/index.js
@@ -1,11 +1,6 @@
 const Sequelize = require('sequelize'); 
 const User = require('./user'); //user파일을 User로 불러옴 
-// const Employee = require('./employee'); //employee파일을 Employee로 불러옴
-// const Music = require('./music');
-// const Pay = require('./pay');
-// const Amount = require('./amount');
-// const PlayHistory = require('./history');
-// const PlayList = require('./playList');
+const Planner = require('./planner')//planner 파일을 Planner로 불러옴
 
 const env = process.env.NODE_ENV || 'development'; //상수 env에 NODE_ENV없으면 'development' 넣음
 const config = require('../config/config')[env]; //상수config에 ../config/config파일에서 env(development) 불러옴
@@ -19,28 +14,15 @@ db.sequelize = sequelize;
 
 //만든 모델들 추가
 db.User = User;
-// db.Employee = Employee;
-// db.Pay = Pay;
-// db.Amount = Amount;
-// db.Music = Music;
-// db.PlayHistory = PlayHistory;
-// db.PlayList = PlayList;
+db.Planner = Planner;
+
 
 User.initiate(sequelize);
-// Employee.initiate(sequelize);
-// Pay.initiate(sequelize);
-// Amount.initiate(sequelize);
-// Music.initiate(sequelize);
-// PlayHistory.initiate(sequelize);
-// PlayList.initiate(sequelize);
+Planner.initiate(sequelize);
+
 
 User.associate(db);
-// Employee.associate(db);
-// Pay.associate(db);
-// Amount.associate(db);
-// Music.associate(db);
-// PlayHistory.associate(db);
-// PlayList.associate(db);
+Planner.associate(db);
 
 
 module.exports = db;
